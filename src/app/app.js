a5a1f6a83c816e861ca84503ae4e5810a2ffe85f
mauyz <tsiorymauyz@gmail.com>
2025-03-08 12:17:20 +0300

    update app services

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 export const bankingAPI = axios.create({
     baseURL: "http://localhost:8085",
@@ -22,4 +23,39 @@ bankingAPI.interceptors.request.use(
 
 export const login = (credentials) => {
     return bankingAPI.post("/auth/login", credentials);
+}
+
+export const logout = () => {
+    return localStorage.removeItem("accessToken");
+}
+
+export const decodeJwt = (token) => {
+    if (token != null) {
+        try {
+            return jwtDecode(token);
+        } catch (error) {
+            console.error('Error decoding JWT:', error);
+        }
+    }
+    else {
+        return null;
+    }
+}
+
+export const decodeRole = (decodedJwt) => {
+    if (decodedJwt) {
+        return decodedJwt.scope.split(" ");
+    }
+    else {
+        return null;
+    }
+}
+
+export const decodeUsername = (decodedJwt) => {
+    if (decodedJwt) {
+        return decodedJwt.sub;
+    }
+    else {
+        return null;
+    }
 }
