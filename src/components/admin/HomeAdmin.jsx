5ce04281eb51159729e21db2463d5278f8b8afa9
mauyz <tsiorymauyz@gmail.com>
2025-03-11 17:08:26 +0300

    implement HomeAdmin component

---
+++ src/components/admin/HomeAdmin.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react'
+import Loader from '../Loader';
+import { deleteCutomer, getCutomers } from '../../app/admin_service';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+export default function HomeAdmin() {
+
+    const navigate = useNavigate();
+
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(
+        () => {
+            getCutomers()
+                .then((response) => {
+                    const list = response.data;
+                    setCustomers(list);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }, []
+    )
+
+    const handleDeleteCustomer = (customer) => {
+        if (confirm(`Do you really delete ${customer.name}`)) {
+            deleteCutomer(customer)
+                .then(() => {
+                    const list = customers.filter(item => item.id != customer.id);
+                    setCustomers(list);
+                })
+        }
+    }
+
+    const handleEditCustomer = (customerId) => {
+        navigate(`/editCustomer/${customerId}`);
+    }
+
+    return (
+        loading
+            ? <Loader />
+            : <div className='col-md-6'>
+                <div className='card mt-3 p-2'>
+                    <div className='card-body'>
+                        <h3 className='my-2'>List of customers</h3>
+                        {
+                            customers.length == 0
+                                ? <h6 className='fst-italic'>No customer registered</h6>
+                                : <table className='table'>
+                                    <thead className='table-light'>
+                                        <tr>
+                                            <td>ID</td>
+                                            <td>Name</td>
+                                            <td>Email</td>
+                                            <td>Action</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            customers.map((item) => {
+                                                return <tr key={item.id}>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.name}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>
+                                                        <button
+                                                            className='btn btn-outline-success me-2'
+                                                            onClick={() => handleEditCustomer(item.id)}>
+                                                            <FontAwesomeIcon icon={faPen}>
+                                                            </FontAwesomeIcon>
+                                                        </button>
+                                                        <button
+                                                            className='btn btn-outline-danger'
+                                                            onClick={() => handleDeleteCustomer(item)}>
+                                                            <FontAwesomeIcon icon={faTrash} />
+                                                        </button>
+                                                    </td>
+                                                </tr>;
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                        }
+                    </div>
+                </div>
+            </div>
+    )
+}
