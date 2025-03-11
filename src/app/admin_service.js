312da899827a66f8307a3592bccca06453824935
mauyz <tsiorymauyz@gmail.com>
2025-03-11 17:10:05 +0300

    implement admin_service

---
+++ src/app/admin_service.js
@@ -0,0 +1,21 @@
+import { apiClient } from "./auth_service"
+
+export const getCutomers = () => {
+    return apiClient().get("/customers");
+}
+
+export const getCustomer = (customerId) => {
+    return apiClient().get(`/customers/${customerId}`);
+}
+
+export const deleteCutomer = (customer) => {
+    return apiClient().delete(`/customers/${customer.id}`);
+}
+
+export const saveCutomer = (customer) => {
+    return apiClient().post('/customers', customer);
+}
+
+export const updateCutomer = (id, customer) => {
+    return apiClient().put(`/customers/${id}`, customer);
+}
